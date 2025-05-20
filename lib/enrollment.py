ff6bf6a57b44899a37da53b41eaa6339e7480fc9
Trevor <nyigeitrevor@gmail.com>
2025-05-20 21:13:21 +0300

    mini

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {}
 
     def enroll(self, course):
         if isinstance(course, Course):
@@ -15,9 +17,22 @@ class Student:
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):
+        return len(self._enrollments)
+
+    def set_grade(self, enrollment, grade):
+        if isinstance(enrollment, Enrollment):
+            self._grades[enrollment] = grade
+        else:
+            raise TypeError("Must be an Enrollment")
+
+    def aggregate_average_grade(self):
+        if not self._grades:
+            return 0
+        return sum(self._grades.values()) / len(self._grades)
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -30,10 +45,9 @@ class Course:
     def get_enrollments(self):
         return self._enrollments.copy()
 
-
 class Enrollment:
     all = []
-    
+
     def __init__(self, student, course):
         if isinstance(student, Student) and isinstance(course, Course):
             self.student = student
@@ -45,3 +59,11 @@ class Enrollment:
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
